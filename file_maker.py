345c0dc0b038d00efebbc41f1515415d86e21a5c
Haotian9850 <haotian9850@gmail.com>
2019-11-09 17:08:20 -0500

    added logging

--- file_maker.py
+++ file_maker.py
@@ -1,4 +1,5 @@
 def make_file(size_kb, file_name, parent_path):
-    with open("{}kb".format(parent_path + file_name + str(size_kb)), "wb") as f:
+    with open("{}{}{}kb".format(parent_path, file_name, str(size_kb)), "wb") as f:
         f.write(str.encode("0") * size_kb * 1024)
+    return "{}{}kb".format(file_name, str(size_kb))
     
--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-import timeit 
-
-
+from upload_benchmarker import benchmark_individual_upload
+from download_benchmarker import benchmark_individual_download
+from config_reader import read_config
 from file_maker import make_file
 
-PARENT_DATA_PATH = "/mnt/documents-local/cloud-service-benchmarker/data/"
-TEST_FILE_NAME = "test"
+import logging 
+logging.basicConfig(
+    filename="benchmarker_log.log",
+    level=logging.INFO,
+    filemode="w"
+)
 
 
-make_file(1, TEST_FILE_NAME, PARENT_DATA_PATH)
 
+CONFIG_FILE_PATH = "config.yaml"
+DATA_PARENT_PATH = "data/"
+DUMP_PATH = "dump/"
+TEST_FILE_NAME_GENERIC = "test"
+
+
+def benchmark():
+    benchmark_configuration = read_config(CONFIG_FILE_PATH)
+    logging.info("Reading config file...")
+    test_files = [make_file(size, TEST_FILE_NAME_GENERIC, DATA_PARENT_PATH) for size in benchmark_configuration["file_size_kb"]]
+    logging.info("--- Beginning of benchmarking ---")
+    for region in benchmark_configuration["regions"]:
+        for file_name in test_files:
+            logging.info("Uploading file {} to S3 in region {}: {}s".format(
+                file_name,
+                region,
+                benchmark_individual_upload(file_name, DATA_PARENT_PATH, region, benchmark_configuration["bucket"])
+            ))
+            logging.info("Downloading file {} from S3 in region {}: {}s".format(
+                file_name,
+                region,
+                benchmark_individual_download(file_name, DATA_PARENT_PATH, region, benchmark_configuration["bucket"])
+            ))
+    logging.info("--- End of benchmarking ---")
+
+
+
+
+if __name__ == "__main__":
+    benchmark()
 
 
 

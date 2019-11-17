053f4cc86d02f71abc7b3cf9dc67109607c03358
Haotian9850 <haotian9850@gmail.com>
2019-11-17 15:14:26 -0500

    added FileMaker

---
+++ parbenchmarker/FileMaker.py
@@ -0,0 +1,10 @@
+
+class FileMaker():
+    def __init__(self):
+        pass 
+
+    def make_test_file(self, size_kb, file_name, parent_path):
+        with open("{}_{}_{}kb".format(parent_path, file_name, str(size_kb)), "wb") as f:
+            f.write(str.encode("0") * size_kb * 1024)
+        return "{}_{}kb".format(file_name, str(size_kb))
+            

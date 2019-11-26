15907724537d3be2cb4e8b39c5785da178810e14
Haotian9850 <haotian9850@gmail.com>
2019-11-26 17:34:37 -0500

    removed redundant packages

--- seqbenchmarker/config_reader.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-def read_config(file_path):
-    result = {}
-    with open(file_path) as config:
-        result = yaml.safe_load(config)
-    return result
--- seqbenchmarker/file_maker.py
+++
@@ -1,5 +0,0 @@
-def make_file(size_kb, file_name, parent_path):
-    with open("{}{}{}kb".format(parent_path, file_name, str(size_kb)), "wb") as f:
-        f.write(str.encode("0") * size_kb * 1024)
-    return "{}{}kb".format(file_name, str(size_kb))
-    

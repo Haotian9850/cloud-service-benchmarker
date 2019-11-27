0536b691fdbccdb7fa24b56cecf99f6cb6ab1fcc
Haotian9850 <haotian9850@gmail.com>
2019-11-26 19:26:01 -0500

    updated main script

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import os
+import time
+
 from RemoteBenchmarker import RemoteBenchMarker
 from Constants import Constants
+
+
 
 START_BENCHMARKING_COMMAND = "python3 parbenchmarker/main.py"
 CLEAN_UP_COMMAND = "rm -r parbenchmarker"
@@ -16,6 +20,19 @@ def upload_source_code(host_name, port, username):
         username
     ))
 
+
+def collect_result(host_name, port, username):
+    print("-------------- collecting benchmark results from host {} --------------".format(host_name))
+    os.system,("scp -P {} -i ~/.ssh/id_geni_ssh_rsa {}@{}:/users/{}/parbenchmarker/{} ./results/{}".format(
+        port,
+        username,
+        host_name,
+        username,
+        "results.csv",
+        "results.csv"
+    ))
+    os.system("mv ./results/results.csv ./results/results_{}.csv".format(host_name))
+    
 
 
 
@@ -40,8 +57,18 @@ if __name__ == "__main__":
             "aws configure set aws_access_key_id {}".format(Constants.AWS_ACCESS_KEY.value),
             "aws configure set aws_secret_access_key {}".format(Constants.AWS_SECRET_KEY.value)
         ])
-        r.exec_command(["touch uploaded"]) #benchmarker start command
+        r.exec_command(["python3 parbenchmarker/main.py &"]) # start benchmarking in detached mode
         r.close_client()
+    
+    time.sleep(Constants.BENCHMARKING_TIME.value)
+
+    for i in range(len(Constants.NODE_HOSTS.value)):
+        collect_result(
+            Constants.NODE_HOSTS.value[i],
+            Constants.NODE_PORTS.value[i],
+            Constants.USERNAME.value
+        )
+    
         
         
 
